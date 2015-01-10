fb1bae43f7cb3640e2d9e4aea28fcaec7590cd69
Sebastian Zapata Mardini <sezama03@hotmail.com>
2015-01-09 23:28:45 -0500

    Add city, state and genre model, fix form to new session

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   has_one :mone
   has_many :transactions
+  belongs_to :state
+  belongs_to :city
+  belongs_to :genre
 end
