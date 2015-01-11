faae3929e24f47ef316bf4cb282c6854153f7a77
mike-casas <neneriostb@gmail.com>
2015-01-11 02:10:47 -0500

    change layout and dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,9 +1,8 @@
 class DashboardController < ApplicationController
   before_action :authenticate_user!
-  layout 'dashboard'
+  layout 'application'
   def index
     @transactions_pie= current_user.transactions.group(:sender_id).count
-
   	@transactions = current_user.transactions
 
   end
