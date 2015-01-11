7017e7f1e9778c63926d285320bc33bbe9e76c7b
mike-casas <neneriostb@gmail.com>
2015-01-11 00:18:14 -0500

    grafic pie in dashboard index

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,7 @@ class DashboardController < ApplicationController
   before_action :authenticate_user!
   layout 'dashboard'
   def index
+    @transactions_pie= current_user.transactions.group(:sender_id).count
   end
 
 
